1abea4322460a4e6fa1dae094c81b2b8d63c3ae7
Peterson W. Santos   :-) <opeterson@hotmail.com>
2017-09-30 17:10:20 -0300

    fix  syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,20 +8,17 @@
 #   include windows_packages
 class windows_packages(
   Boolean                     $choco_install_location_manage     = false ,
-  String                      $choco_install_location_path       = 'C:\ProgramData\chocolatey',
+  Stdlib::Absolutepath        $choco_install_location_path       = 'C:\ProgramData\chocolatey'
   Boolean                     $choco_install_log_output          = false ,
   Boolean                     $chocolateyconfig_proxy_manage     = false ,
-  String                      $chocolateyconfig_proxy_server     = 'server_ip',
-  String                      $chocolateyconfig_proxyuser        = 'proxy_user',
-  String                      $chocolateyconfig_proxypassword    = 'proxy_password',
+  String                      $chocolateyconfig_proxy_server     = undef ,
+  String                      $chocolateyconfig_proxyuser        = undef ,
+  String                      $chocolateyconfig_proxypassword    = undef ,
   Enum['enabled','disabled']  $chocolateyfeature_autouninstaller = enabled ,
-  Boolean                      $chocolateyfeature_viruscheck     = false   ,
+  Bolean                      $chocolateyfeature_viruscheck      = false   ,
   Boolean                     $package_manage                    = true  ,
   Hash                        $package_list                      = {}    ,
 ){
-
-  contain stdlib
-
   if $::kernel == 'windows' {
 
     # Ensure Chocolatey is installed and configured:
@@ -29,11 +26,11 @@ class windows_packages(
 
     Package { provider => chocolatey, }
 
-    contain windows_packages::config
-    contain windows_packages::install
+    contain desktop_packages::config
+    contain desktop_packages::install
 
-    Class['::windows_packages::config']
-    -> Class['::windows_packages::install']
+    Class['::desktop_packages::config']
+    -> Class['::desktop_packages::install']
 
   }
 
